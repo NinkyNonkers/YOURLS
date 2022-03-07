d7cf6436231adbce6a1700083d62fed6c444e4ea
archie426 <archiestride42@gmail.com>
2022-03-07 21:00:18 +0000

    REMOVE BRANDING

--- includes/functions-html.php
+++ includes/functions-html.php
@@ -5,16 +5,6 @@
  *
  */
 function yourls_html_logo() {
-	yourls_do_action( 'pre_html_logo' );
-	?>
-	<header role="banner">
-	<h1>
-		<a href="<?php echo yourls_admin_url( 'index.php' ) ?>" title="YOURLS"><span>YOURLS</span>: <span>Y</span>our <span>O</span>wn <span>URL</span> <span>S</span>hortener<br/>
-		<img src="<?php yourls_site_url(); ?>/images/yourls-logo.svg?v=<?php echo YOURLS_VERSION; ?>" id="yourls-logo" alt="YOURLS" title="YOURLS" /></a>
-	</h1>
-	</header>
-	<?php
-	yourls_do_action( 'html_logo' );
 }
 
 /**
@@ -149,13 +139,7 @@ function yourls_html_footer($can_query = true) {
 
 	?>
 	</div><?php // wrap ?>
-	<footer id="footer" role="contentinfo"><p>
-		<?php
-		$footer  = yourls_s( 'Powered by %s', '<a href="http://yourls.org/" title="YOURLS">YOURLS</a> v ' . YOURLS_VERSION );
-		$footer .= $num_queries;
-		echo yourls_apply_filter( 'html_footer_text', $footer );
-		?>
-	</p></footer>
+
 	<?php if( yourls_get_debug_mode() ) {
 		echo '<div style="text-align:left"><pre>';
 		echo join( "\n", yourls_get_debug_log() );
